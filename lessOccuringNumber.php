dbfb6ee0d93db88cbd868245279e3b5dcddd7b97
benzics <benjaminnicholas29@gmail.com>
2023-01-17 09:14:19 +0100

    refactored my solution

--- lessOccuringNumber.php
+++ lessOccuringNumber.php
@@ -2,28 +2,14 @@
 
 // $numArray = [1, 1, 2, 3, 3, 1, 2, 4, 5, 4, 6, 5, 7, 7]
 // write a code to find number with less occurence.
-// time complexity: O(n)
 
 function lessOccurence(array $items) : int
 {
-    // sort($items);
-
     $itemCount = array_count_values($items);
 
-    // for($i = 0; $i < count($items); $i++) {
+    asort($itemCount); 
 
-    //     if(!isset($itemCount[$items[$i]])) $itemCount[$items[$i]] = 1;
-
-    //     if(isset($items[$i + 1]) && $items[$i + 1] === $items[$i]) {
-    //         $itemCount[$items[$i]]++;
-    //         continue;
-    //     }
-
-    // }
-
-    $min = array_keys($itemCount, min($itemCount));
-
-    return $min[0];
+    return array_key_first($itemCount);
 }
 
 echo lessOccurence([1, 1, 2, 3, 3, 1, 2, 4, 5, 4, 6, 5, 7, 7]);
